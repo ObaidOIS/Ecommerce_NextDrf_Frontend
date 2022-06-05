7fee062fd2f96e70a52f52e6a085971eebbbb0e7
ObaidOIS <obaidch00@gmail.com>
2022-06-05 16:16:34 +0500

    fetching_from_api

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,36 @@
 import { makeStyles } from '@material-ui/core/styles';
+import Header from '../components/header';
 
 const useStyles = makeStyles((theme) => ({
   example: {
-    color: "fff",
+    color: "#ccc",
   }
 }));
 
-export default function Home() {
+function Home({ posts }) {
 
   const classes = useStyles();
 
   return (
-    <div className={classes.example}>
-      hello
-    </div>
+    <>
+      <Header />
+      {console.log(posts)}
+      <div className={classes.example}>
+        {posts.map((post) => (
+          <p>{post.title}</p>
+        ))}
+      </div>
+    </>
   )
 }
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:8000/api/")
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts
+    }
+  };
+}
+export default Home
